a1944b99684cc63985f2fb77db98ad4663237ecd
JT <jtapiaelenes24@gmail.com>
2024-08-28 19:25:39 +0200

    Inicio del curso, añadidos archivos index.js, static/ index.html

---
+++ index.js
@@ -0,0 +1,26 @@
+
+// *** SERVIDOR CON NODE ***
+// const http = require('http')
+// const fs = require('fs')
+
+// const server = http.createServer((req, res) => {
+//    const read = fs.createReadStream('./static/index.html')
+//    read.pipe(res)
+// })
+
+// server.listen(3000)
+// console.log(`Server on port ${3000}`)
+
+// *** SERVIDOR CON EXPRESS ***
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile('./static/index.html', {
+        root: __dirname
+    })
+})
+
+app.listen(3000)
+console.log(`Server on port ${3000}`)
